365f2379bebfee96486f4234ba052dd72047599c
Josh G <m0rtiferrimus@gmail.com>
2013-01-26 23:16:11 -0600

    First real commit.
    Messed up on the first commit. Now I've taken out files that were
    generated by the scripts, ensuring that scripts you download will work
    properly on your computer.

---
+++ batch/hack.bat
@@ -0,0 +1,65 @@
+@echo off
+echo Starting batch...
+echo Trying to get root access, hold on...
+:root
+cd C:\
+echo %UserProfile%, do you have root access? (y/n)
+@echo on
+set /p userin1=
+@echo off
+if '%userin1%'=='y' goto exit
+if '%userin1%'=='Y' goto exit
+if '%userin1%'=='n' goto step2
+if '%userin1%'=='N' goto step2
+echo That's not a 'y' or an 'n'. Let's try this again...
+goto root
+:step2
+cls
+echo Ok, trying to open cmd, just a second...
+cmd
+echo If it is not blocked on this PC, cmd.exe ought to be open. Is it? (y/n)
+set /p userin2=
+if '%userin2%'=='y' goto exit
+if '%userin2%'=='Y' goto exit
+if '%userin2%'=='n' goto step3
+if '%userin2%'=='N' goto step3
+echo Invalid input.
+goto step2
+:step3
+cls
+echo You're under some tight security, then.
+echo Let's try getting to the control panel...
+control panel
+echo Did that work? (y/n)
+set /p userin3=
+if '%userin3%'=='y' goto exit
+if '%userin3%'=='Y' goto exit
+if '%userin3%'=='n' goto step4
+if '%userin3%'=='N' goto step4
+echo Invalid input.
+goto step3
+:step4
+cls
+echo Whew...
+echo Ok, time for cmd mode. This function will cause the script
+echo to be a replacement for the built-in cmd.exe. It will
+echo pass commands on to the kernel just like cmd.exe.
+pause
+:step5
+cls
+echo Enter cmd mode? (y/n)
+set /p userin4=
+if '%userin4%'=='y' goto cmdmode
+if '%userin4%'=='Y' goto cmdmode
+if '%userin4%'=='n' goto exit
+if '%userin4%'=='N' goto exit
+echo Invalid input.
+goto step5
+:cmdmode
+set /p userin5=
+%userin5%
+goto cmdmode
+:exit
+echo Thanks for using this script!
+pause
+exit
---
+++ batch/login.bat
@@ -0,0 +1,29 @@
+@echo off
+title Instructables Program
+color 1f
+:start
+echo Do you already have an account? (y/n)
+set /p userin1=
+if '%userin1%'=='n' goto newuser
+if '%userin1%'=='N' goto newuser
+if '%userin1%'=='y' goto user
+if '%userin1%'=='Y' goto user
+echo That's not a 'y' or an 'n'. Let's try this again:
+goto start
+:newuser
+set /p username=Please enter your username:
+set /p password=Please enter your password:
+echo %username%>username.txt
+echo %password%>password.txt
+:user
+set /p userin2=<username.txt
+set /p userin3=<password.txt
+set /p userin4=Username:
+set /p userin5=Password:
+if '%userin4%'=='%userin2%' goto password
+if not '%userin4%'=='%userin2%' goto user
+:password
+if '%userin5%'=='%userin3%' goto begin
+if not '%userin5%'=='%userin3%' goto user
+:begin
+echo Welcome, %userin2%!
